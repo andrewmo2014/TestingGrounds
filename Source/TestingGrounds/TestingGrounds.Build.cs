9cdf37e2656df99aa415b5b7fee0e40369715a95
Andrew Moran <andrewmo@mit.edu>
2016-12-13 19:21:46 -0800

    Updated gitignore

--- Source/TestingGrounds/TestingGrounds.Build.cs
+++ Source/TestingGrounds/TestingGrounds.Build.cs
@@ -6,7 +6,7 @@ public class TestingGrounds : ModuleRules
 {
 	public TestingGrounds(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "GameplayTasks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
