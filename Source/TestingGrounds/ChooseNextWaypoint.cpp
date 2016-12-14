21651b20827a07bc48b19f6bf5c6f5e4e063ea52
Andrew Moran <andrewmo@mit.edu>
2016-12-13 22:48:18 -0800

    Reading blackboard data

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -2,11 +2,15 @@
 
 #include "TestingGrounds.h"
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("AI in C++"));
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+
+	//UE_LOG(LogTemp, Warning, TEXT("AI in C++"));
 	return EBTNodeResult::Succeeded;
 }
 
