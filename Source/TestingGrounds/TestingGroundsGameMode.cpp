ae0d2bef900c6ed8ecc8eee63610ccc6cfafac3f
Andrew Moran <andrewmo@mit.edu>
2016-12-29 14:30:45 -0800

    Fixed unlinked refs, added gun to third person ue4 character

---
+++ Source/TestingGrounds/TestingGroundsGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "TestingGrounds.h"
+#include "TestingGroundsGameMode.h"
+#include "TestingGroundsHUD.h"
+#include "Player/FirstPersonCharacter.h"
+
+AS05_TestingGroundsGameMode::AS05_TestingGroundsGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = AS05_TestingGroundsHUD::StaticClass();
+}
